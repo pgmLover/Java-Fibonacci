3c19b9f3e1c37a20add76ba8b6c0f00254a75feb
pgmLover <anandakrishnapb11@gmail.com>
2022-04-18 13:11:04 +0530

    Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("Fibonacci Series\n");
+        System.out.print("Enter the count:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+
+        int a=0;
+        int b=1;
+        System.out.print("Series is: ");
+        System.out.print(a);
+        System.out.print(","+b);
+
+        for(int i=2;i<n;i++)
+        {
+           int c=a+b;
+            a=b;
+            b=c;
+            System.out.print(","+c);
+
+
+        }
     }
 }
